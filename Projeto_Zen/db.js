d9da2d74f43ce504873d06d511b1182f66f2350f
Douglas-00 <alvesdouglas547@gmail.com>
2021-11-30 18:21:50 -0300

    ...

--- Projeto_Zen/db.js
+++ Projeto_Zen/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql')
 
 const connection = mysql.createConnection({
-    host: '3306',
+    host: 'localhost',
     user: 'root',
     password: '12345678',
     database: 'projeto_uni'
